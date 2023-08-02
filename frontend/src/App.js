04ee8b747c942ea1a54dc2619412f3bb96a767ad
aquibsayyed42 <54478891+aquibsayyed42@users.noreply.github.com>
2023-08-02 19:33:54 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ import Thanks from "./components/Thanks";
 import ipConfig from "./ipConfig.json";
 
 export const config = {
-  endpoint: `https://qkart-backend-kb1y.onrender.com`,
+  endpoint: `https://qkart-backend-kb1y.onrender.com/v1`,
 };
 
 export default function App(props) {
